0357246cdf3ae43a367fee4f6e01fb567a1adc6f
iXploitID <93501879+iXploitID@users.noreply.github.com>
2021-11-02 16:05:05 +0700

    Add files via upload

---
+++ gas.sh
@@ -0,0 +1,54 @@
+#
+#
+clear
+echo -e "CODED BY :"
+
+#
+#
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo -e $red"    ██     ▄▄▄  ▄▄▄            ▄▄▄▄                   ██       ██"
+echo -e $red"    ▀▀      ██▄▄██             ▀▀██                   ▀▀       ██"
+echo -e $red"  ████       ████    ██▄███▄     ██       ▄████▄    ████     ███████"
+echo -e $red"    ██        ██     ██▀  ▀██    ██      ██▀  ▀██     ██       ██"
+echo -e $white"    ██       ████    ██    ██    ██      ██    ██     ██       ██"
+echo -e $white" ▄▄▄██▄▄▄   ██  ██   ███▄▄██▀    ██▄▄▄   ▀██▄▄██▀  ▄▄▄██▄▄▄    ██▄▄▄"
+echo -e $white" ▀▀▀▀▀▀▀▀  ▀▀▀  ▀▀▀  ██ ▀▀▀       ▀▀▀▀     ▀▀▀▀    ▀▀▀▀▀▀▀▀     ▀▀▀▀"
+echo -e $white"                     ██"
+echo
+echo
+echo
+echo -e $red "########################################"
+echo -e $red "# CREATOR: i-Xploit                    #"
+echo -e $red "# Yt: Ramdom Toturial                  #"
+echo -e $white "#jangan recrod script ini            #"
+echo -e $white"#Github :https://github.com/iXploitID #"
+echo -e $white "######################################"
+echo
+echo
+echo -e $blue"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "1.LANJUT KE TOOLS INSTALER NYA"
+echo -e $blue"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "2.Cek yt creator"
+echo -e $blue"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo
+echo
+read -p "[masukannomer]> " act;
+
+if [ $act = 2 ] || [ $act = 2 ]
+then
+cleat
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+exit
+fi
+
+if [ $act = 1 ] || [ $act = 1 ]
+then
+bash run.sh
+clear
+fi
---
+++ run.sh
@@ -0,0 +1,1123 @@
+#SELAMAT DATANG BANGSTA_-
+#JAN REVROD INI SC
+#CAPE GW BUAT NYA MAK :V
+#AHIK KANG RECROD BAJINGNA JANCOK ASW SLEWBEM :V#
+#
+#
+#
+
+#
+
+#
+
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+#
+
+#
+#
+
+
+#
+#
+
+#
+#
+#
+#
+#
+#
+
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+#
+#
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+#
+
+#
+#
+#
+
+#
+
+#
+#
+
+#
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+
+#
+
+#
+#
+#
+#
+#
+#
+#
+
+#
+
+#
+#
+
+#
+#
+#
+
+#
+
+#
+#
+
+#
+
+#
+#
+
+#
+#
+
+#
+##
+#
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+#
+
+
+#
+#
+
+#
+#
+#
+
+#
+#
+#
+#
+
+#
+
+#
+
+#
+#
+
+#
+
+#
+#
+
+
+
+##
+#
+
+#
+
+#
+#
+
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+#
+#
+#
+#
+
+#
+#
+
+#
+#
+#
+
+#
+
+
+#
+#
+
+#
+#
+
+#
+
+#
+
+#
+#
+
+#
+
+#
+#
+
+#
+#
+#
+#
+
+#
+#
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+
+
+#
+
+#
+#
+#
+
+#
+#
+
+#
+##
+
+#
+#
+#
+#
+#
+#
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+
+#
+
+#
+#
+#
+#
+
+#
+
+#
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+#
+#
+#
+
+#
+#
+
+#
+
+#
+#
+#
+
+#
+#
+
+#
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+#
+
+#
+#
+#
+
+#
+#
+
+#
+#
+#
+
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+clear
+##
+#
+#
+#
+
+echo -e "CODED BY :"#
+
+#
+#
+echo -e $red"    ██     ▄▄▄  ▄▄▄            ▄▄▄▄                   ██       ██"
+echo -e $red"    ▀▀      ██▄▄██             ▀▀██                   ▀▀       ██"
+echo -e $red"  ████       ████    ██▄███▄     ██       ▄████▄    ████     ███████"
+echo -e $red"    ██        ██     ██▀  ▀██    ██      ██▀  ▀██     ██       ██"
+echo -e $white"    ██       ████    ██    ██    ██      ██    ██     ██       ██"
+echo -e $white" ▄▄▄██▄▄▄   ██  ██   ███▄▄██▀    ██▄▄▄   ▀██▄▄██▀  ▄▄▄██▄▄▄    ██▄▄▄"
+echo -e $white" ▀▀▀▀▀▀▀▀  ▀▀▀  ▀▀▀  ██ ▀▀▀       ▀▀▀▀     ▀▀▀▀    ▀▀▀▀▀▀▀▀     ▀▀▀▀"
+echo -e $white"                     ██"
+echo
+echo
+echo
+echo -e $red "########################################"
+echo -e $red "# CREATOR: i-Xploit                    #"
+echo -e $red "# Yt: Ramdom Toturial                  #"
+echo -e $white "#jangan recrod script ini            #"
+echo -e $white"#Github :https://github.com/iXploitID #"
+echo -e $white "######################################"
+echo
+echo
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "1.>V-LINK"
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "2.>IPCS"
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "3.>DDOS HULK"
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "4.DEFACE UP SCRIPT"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "5.> Install SocialFish Buat Phising"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "6.> Install MoonBuggy Buat Main Game"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "7.> Install Cupp Buat Worldlist Tertarget"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "8.> Install Hydra"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "9.> Install md5-crack"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "10.> Install LITESPAM Buat Spam"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "11.> Mulung Bitcoin"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "12.> Install Ko-Dork"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "13.> Lihat Spesifikasi Ponsel"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "14.> install lhst"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "15.> Install The Fat Rat"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "16.> Install CMSmap"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "17.> Install Kali Ubuntu"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "18.> Install Ngrok"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "19.> Install Brute Force Gmail"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "20.> Install Lokomedia Exploiters"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "21.> Install Kali Linux Nethunter"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "22.> Install joomscan"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "23.> Install VulnScaner"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "24.> Install HN-Installer"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "25.> Install Fucking-Rat"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "26.> Install linux fedora"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "27.> Install information Gathering"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $blue "28.> Install ezsploit [Harus Root]"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "29.> Install Dirsearch "
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "30.> Install honeypot"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "31.> Install Killr"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $blue "32.> Install Human_Level_Control"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "33.> Install Termux-Mpv"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "34.> Install OWASP-Nettacker"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "35.> Install OWASP-ZSC"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "36.> Install InfoGa"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "37.> Install cok-rat"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $blue "38.> Install hash-Generator"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "39.> Install Open Source Information Facebook"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $yellow "40.> Install str2bf"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "41.> Install BForce"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "42.> Install Web Application Audit Framework"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "43.> Install SMBrute"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "44.> Install Wordpress Security Scanner"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $blue "45.> Install hmap (fingerprinting web servers)"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $yellow "46.> Install Auto Report Facebook"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "47.> Install dhcpdorf"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "48.> Install deskcon-desktop"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "49.> Install Tools suRAT-Cinta"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $blue  "50.> Install Tools m4ngARP"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e   " 000.>EXIT/KELUAR DARI TOOLS"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo
+echo
+echo -e $red "MASUKAN NOMER PILIHAN MU"
+echo
+read -p "[SUSCRIBE]> " act;
+
+if [ $act = 1 ] || [ $act = 1 ]
+then
+pkg update && pkg upgrade
+pkg install figlet
+pkg install git
+git clone https://github.com/MrVirusSpm-07/link-virus
+cd link-virus
+sh download.sh
+fi
+
+if [ $act = 2 ] || [ $act = 2 ]
+then
+pkg update && pkg upgrade 
+pkg install python2 -y
+pkg install git -y
+git clone https://github.com/Kancotdiq/ipcs
+cd ipcs
+pip2 install requests
+python2 scan.py
+
+fi
+
+if [ $act = 3 ] || [ $act = 3 ]
+then
+apt update && upgrade
+pkg install git
+pkg install python
+git clone https://github.com/Mr4FX/HULK
+cd HULK
+chmod +x hulk.py
+python hulk.py
+
+fi
+
+if [ $act = 4 ] || [ $act = 4 ]
+then
+pkg update && pkg upgrade
+pkg install figlet
+pkg install git
+git clone https://github.com/MrVirusSpm-07/upload-02
+cd upload-02
+sh depes02.sh
+fi
+
+if [ $act =  5 ] || [ $act = 5 ]
+then
+
+pkg install php
+pkg install python2
+pkg install git
+pkg install curl
+git clone https://github.com/Lexiie/SocialFish.git
+mv SocialFish $HOME
+cd $HOME/SocialFish
+pip2 install wget
+python2 SocialFish.py
+fi
+
+if [ $act = 6 ] || [ $act = 6 ]
+then
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $act = 7 ] || [ $act = 7 ]
+then
+pkg install git
+pkg install python2
+git clone https://github.com/mebus/cupp.git
+mv cupp $HOME
+cd $HOME/cupp
+python2 cupp.py
+fi
+
+if [ $act = 8 ] || [ $act = 8 ]
+then
+
+pkg install python2
+pkg install python
+pkg install php
+pkg install hydra
+fi
+
+if [ $act = 9 ] || [ $act = 9 ]
+then
+
+pkg install git
+pkg install python2
+git clone https://github.com/CiKu370/md5-crack.git
+mv md5-crack $HOME
+cd $HOME/md5-crack
+python2 md5.py
+fi
+
+if [ $act = 10 ] || [ $act = 10 ]
+then
+pkg install python2
+pkg install php
+pkg install cowsay
+pkg install toilet
+pkg install ruby
+gem install lolcat
+pkg install git
+git clone https://github.com/4L13199/LITESPAM.git
+mv LITESPAM $HOME
+cd $HOME/LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $act = 11 ] || [ $act = 11 ]
+then
+
+pkg install python
+pkg install git
+pkg install php
+git clone https://github.com/Cvar1984/pemulungBTC.git
+mv pemulungBTC $HOME
+cd $HOME/pemulungBTC
+php autoload.php
+php mulung.php
+fi
+
+if [ $act = 12 ] || [ $act = 12 ]
+then
+
+pkg update && pkg upgrade
+pkg install git 
+pkg install python2
+git clone https://github.com/ciku370/ko-dork.git
+mv ko-dork $HOME
+cd $HOME/ko-dork
+python2 dork.py
+fi
+
+if [ $act = 13 ] || [ $act = 13 ]
+then
+pkg install neofetech
+neofetech
+fi
+
+if [ $act = 14 ] || [ $act = 14 ]
+then
+
+pkg install python2
+pkg install git
+git clone https://github.com/CiKu370/lhst
+mv lhst $HOME
+cd $HOME/lhst
+python2 lhst.py
+fi
+
+if [ $act = 15 ] || [ $act = 15 ]
+then
+apt-get update && apt-get upgrade
+apt-get install git
+git clone https://github.com/Screetsec/TheFatRat.git
+mv TheFatRat $HOME
+cd $HOME/TheFatRat
+chmod +x setup.sh 
+./setup.sh
+fi
+
+if [ $act = 16 ] || [ $act = 16 ]
+then
+apt-get update 
+apt-get upgrade
+pkg install python
+apt-get install git
+git clone https://github.com/Dionach/CMSmap.git
+mv CMSmap $HOME
+cd $HOME/CMSmap
+ls
+fi
+
+if [ $act = 17 ] || [ $act = 17 ]
+then
+
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+mv termux-ubuntu $HOME
+cd $HOME/~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+fi
+if [ $act = 18 ] || [ $act = 18 ]
+then
+
+apt update && apt upgrade
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+mv ngrok-stable-linux-arm.zip $HOME
+$HOME/unzip ngrok-stable-linux-arm.zip
+cd ~/
+ls
+fi
+
+if [ $act = 19 ] || [ $act = 19 ]
+then
+
+apt update && apt upgrade
+apt install git
+git clone https://github.com/JamesAndresCM/Brute_force_gmail
+mv Brute_force_gmail $HOME
+cd $HOME/Brute_force_gmail
+ls
+fi
+
+if [ $act = 20 ] || [ $act = 20 ]
+then
+
+apt update && apt upgrade
+pkg install php
+pkg install wget
+wget https://pastebin.com/raw/sPpJRjCZ -O lokomedia.php
+mv lokomedia.php $HOME
+cd
+php lokomedia.php 
+fi
+
+if [ $act = 21 ] || [ $act = 21 ]
+then
+
+
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+mv Nethunter-In-Termux $HOME
+cd $HOME/Nethunter-In-Termux
+chmod 117 kalinethunter
+sh kalinethunter
+fi
+
+if [ $act = 22 ] || [ $act = 22 ]
+then
+
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/rezasp/joomscan.git
+mv joomscan $HOME
+cd $HOME/joomscan
+ls
+fi
+if [ $act = 23 ] || [ $act = 23 ]
+then
+
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/kereh/VulnScaner
+mv VulnScaner $HOME
+cd $HOME/VulnScaner
+python2 VulnScaner.py
+fi
+
+if [ $act = 24 ] || [ $act = 24 ]
+then
+
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/kereh/HN-Installer
+mv HN-Installer $HOME
+cd $HOME/HN-Installer
+python2 HN-Install.py
+fi
+
+if [ $act = 25 ] || [ $act = 25 ]
+then
+
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/kereh/Fucking-Rat
+mv Fucking-Rat $HOME
+cd $HOME/Fucking-Rat
+python2 fr.py
+fi
+
+if [ $act = 26 ] || [ $act = 26 ]
+then
+
+echo -e  -e $green" installing fedora "
+sleep 1
+apt update && apt upgrade
+apt-get install git
+apt install wget
+git clone https://github.com/nmilosev/termux-fedora.git
+mv termux-fedora $HOME
+cd $HOME/termux-fedora
+chmod +x termux-fedora.sh
+sh termux-fedora.sh
+fi
+
+if [ $act = 27 ] || [ $act = 27 ]
+then
+
+aptupdate
+apt upgrade
+pkg install python
+apt install git
+git clone https://github.com/m4ll0k/Infoga.git infoga
+sleep 1
+mv infoga $HOME
+cd $HOME/infoga
+pip install -r req
+python2 infoga.py
+fi
+
+if [ $act = 28 ] || [ $act = 28 ]
+then
+
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/rand0m1ze/ezsploit
+mv ezsploit $HOME
+cd $HOME/ezsploit
+bash ezsploit.sh
+fi
+
+if [ $act = 29 ] || [ $act = 29 ]
+then
+
+apt update
+apt upgrade
+apt install python
+apt install git
+git clone https://github.com/maurosoria/dirsearch.git
+mv  dirsearch $HOME
+cd $HOME/dirsearch
+ls
+fi
+
+if [ $act = 30 ] || [ $act = 30 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install php
+git clone https://github.com/whackashoe/php-spam-mail-honeypot.git
+mv php-spam-mail-honeypot $HOME
+cd $HOME/php-spam-mail-honeypot
+php honeypot.php
+fi
+if [ $act = 31 ] || [ $act = 31 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install php
+git clone https://github.com/whackashoe/killr.git
+mv killr $HOME
+cd $HOME/killr
+php server.php
+fi
+
+if [ $act = 32 ] || [ $act = 32 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install php
+git clone https://github.com/whackashoe/Human_Level_Control_through_Deep_Reinforcement_Learning.git
+mv Human_Level_Control_through_Deep_Reinforcement_Learning $HOME
+cd $HOME/Human_Level_Control_through_Deep_Reinforcement_Learning
+sh install_dependencies.sh
+fi
+
+if [ $act = 33 ] || [ $act = 33 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install php
+git clone https://github.com/Neo-Oli/Termux-Mpv.git
+mv Termux-Mpv $HOME
+cd $HOME/Termux-Mpv
+python2 setup.py 
+fi
+
+if [ $act = 34 ] || [ $act = 34 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python
+git clone https://github.com/zdresearch/OWASP-Nettacker
+mv OWASP-Nettacker $HOME
+cd $HOME/OWASP-Nettacker
+python2 setup.py 
+fi
+
+if [ $act = 35 ] || [ $act = 35 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/zdresearch/OWASP-ZSC
+mv OWASP-ZSC $HOME
+cd $HOME/OWASP-ZSC
+python2 installer.py 
+fi
+
+if [ $act = 36 ] || [ $act = 36 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install php
+git clone https://github.com/mrcakil/InfoGa
+mv InfoGa $HOME
+cd $HOME/InfoGa
+php infoga.php
+fi
+if [ $act = 37 ] || [ $act = 37 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+git clone https://github.com/mrcakil/cok-Rat
+mv cok-Rat $HOME
+cd $HOME/cok-Rat
+unzip rat.zip
+ls
+fi
+
+if [ $act = 38 ] || [ $act = 38 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/CiKu370/hash-generator
+mv hash-generator $HOME
+cd $HOME/hash-generator
+python2 hashgen.py
+fi
+
+if [ $act = 39 ] || [ $act = 39 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/CiKu370/OSIF
+mv OSIF $HOME
+cd $HOME/OSIF
+python2 osif.py
+fi
+
+if [ $act = 40 ] || [ $act = 40 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/CiKu370/str2bf
+mv str2bf $HOME
+cd $HOME/str2bf
+python2 str2bf.py
+fi
+if [ $act = 41 ] || [ $act = 41 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/YukersCreew/BForce
+mv BForce $HOME
+cd $HOME/BForce
+python2 Yukers.py
+fi
+
+if [ $act = 42 ] || [ $act = 42 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/m4ll0k/Galileo
+mv Galileo $HOME
+cd $HOME/Galileo
+pip2 install -r requirements.txt
+python2 galileo.py
+fi
+
+if [ $act = 43 ] || [ $act = 43 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/m4ll0k/SMBrute
+mv SMBrute $HOME
+cd $HOME/SMBrute
+python2 smbrute.py
+fi
+
+if [ $act = 44 ] || [ $act = 44 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/m4ll0k/WPSeku
+mv WPSeku $HOME
+cd $HOME/WPSeku
+pip2 install -r requirements.txt
+python2 wpseku.py
+fi
+
+if [ $act = 45 ] || [ $act = 45 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/Mebus/hmap
+mv hmap $HOME
+cd $HOME/hmap
+python2 hmap.py -h
+fi
+
+if [ $act = 46 ] || [ $act = 46 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install ruby
+git clone https://github.com/gshofficialgithubindonesia/autoreport-fb
+mv autoreport-fb $HOME
+cd $HOME/autoreport-fb
+ruby autoreport-fb.rb
+fi
+
+if [ $act = 47 ] || [ $act = 47 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install golang
+git clone https://github.com/Mebus/dhcpdorf
+mv dhcpdorf $HOME
+cd $HOME/dhcpdorf
+golang dhcpdorf.go
+fi
+
+if [ $act = 48 ] || [ $act = 48 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/Mebus/deskcon-desktop
+mv deskcon-desktop $HOME
+cd $HOME/deskcon-desktop
+sh deskcon.sh
+fi
+
+if [ $act = 49 ] || [ $act = 49 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+git clone https://github.com/aryanrtm/suRAT-cinta
+mv suRAT-cinta $HOME
+cd $HOME/suRAT-cinta
+sh suRAT.sh
+fi
+
+if [ $act = 50 ] || [ $act = 50 ]
+then
+
+apt update 
+apt upgrade
+apt install git 
+git clone https://github.com/aryanrtm/m4ngARP
+mv m4ngARP $HOME
+cd $HOME/m4ngARP
+sh m4ngARP.sh
+fi
+if [ $act = 00 ] || [ $act = 00 ]
+then
+toilet -f mono12 -F metal SUSBCRIBE
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+toilet -f mono12 -F metal Bay
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+sleep 3
+clear
+exit
+fi
